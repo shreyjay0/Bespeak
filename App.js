4418b76d3eca13d1048ddb99fcb19774ddfa972e
Shreyansh <shreyjay0@gmail.com>
2020-04-26 14:20:07 +0530

    views

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Contants from 'expo-constants';
+import {home} from './views/home'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bespeak</Text>
     </View>
   );
 }
@@ -13,7 +14,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
 });
+
+
+
