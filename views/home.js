6f103473891ec8f8ffb39243c166f2a36ef0ae95
Shreyansh <shreyjay0@gmail.com>
2020-04-26 14:21:36 +0530

    views added

---
+++ views/home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Contants from 'expo-constants';
+
+
+const Home=() => {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontSize: 15}}>Home</Text>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+  margin: 8,
+  backgroundColor: '#333',
+    alignItems: 'center'
+  },
+});
+
+export {home};
