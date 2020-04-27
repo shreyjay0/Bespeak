40287239fed85f2312648447715814ba98de268a
Shreyansh <shreyjay0@gmail.com>
2020-04-27 22:29:31 +0530

    updates

---
+++ Screens/login.js
@@ -0,0 +1,14 @@
+/* Core */
+import React, { Component } from 'react';
+/* Presentational */
+import { View, Text, Stylesheet } from 'react-native';
+
+// import styles from './styles';
+
+export default class login extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
